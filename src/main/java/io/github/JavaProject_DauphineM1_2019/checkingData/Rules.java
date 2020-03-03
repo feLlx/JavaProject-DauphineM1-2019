e937c4331c9e017c9d77c2129a711f6ca32e7409
feLlx <vitasfelix@gmail.com>
2020-03-04 00:36:31 +0100

    Correction in Rules class and javaDoc and creation of Rules test class

--- src/main/java/io/github/JavaProject_DauphineM1_2019/checkingData/Rules.java
+++ src/main/java/io/github/JavaProject_DauphineM1_2019/checkingData/Rules.java
@@ -3,14 +3,24 @@ package io.github.JavaProject_DauphineM1_2019.checkingData;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import io.github.JavaProject_DauphineM1_2019.App;
-
 public class Rules {
 
+	/**
+	 * This method check if the parameter is a String
+	 * 
+	 * @param data
+	 * @return
+	 */
 	public boolean STRING(String data) {
 		return data instanceof String;
 	}
-	
+
+	/**
+	 * This method check if the parameter is an Integer
+	 * 
+	 * @param data
+	 * @return
+	 */
 	public boolean INT(String data) {
 		try {
 			Integer.parseInt(data);
@@ -19,28 +29,42 @@ public class Rules {
 		}
 		return true;
 	}
-	
+
+	/**
+	 * This method check if the parameter is an Integer and in the interval ]0;120[
+	 * 
+	 * @param data
+	 * @return
+	 */
 	public boolean BE_AN_AGE(String data) {
-		try {
-			int value = Integer.parseInt(data);
-			return value > 0 && value < 120;
-		} catch (Exception e) {
-			return false;
-		}
+		return this.INT(data) && Integer.parseInt(data) > 0 && Integer.parseInt(data) < 120;
 	}
-	
+
+	/**
+	 * This method check if the parameter respect the regEx of a mail (XXX@YYY.ZZ)
+	 * 
+	 * @param data
+	 * @return
+	 */
 	public boolean BE_AN_EMAIL(String data) {
 		String regex = "^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z]+\\.)+[a-zA-Z]{2,6}$";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher mat = pattern.matcher(data);
 		return mat.matches();
 	}
-	
+
+	/**
+	 * This method check if the parameter respect the regEx of a dauphine mail
+	 * (XXX@dauphine.eu or XXX@dauphine.psl.eu or XXX@lamsade.dauphine.fr)
+	 * 
+	 * @param data
+	 * @return
+	 */
 	public boolean BE_AN_DAUPHINE_EMAIL(String data) {
 		String regexDauphine = "^(([\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(dauphine.eu|dauphine.psl.eu|lamsade.dauphine.fr)))$";
 		Pattern pattern = Pattern.compile(regexDauphine);
 		Matcher mat = pattern.matcher(data);
 		return mat.matches();
 	}
-	
+
 }
