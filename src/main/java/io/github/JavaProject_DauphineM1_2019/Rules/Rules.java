6005a332f07fcc836251d0a97b1217c4069cb57a
feLlx <vitasfelix@gmail.com>
2020-03-04 01:38:21 +0100

    Correction anonymize method and javadoc

--- src/main/java/io/github/JavaProject_DauphineM1_2019/Rules/Rules.java
+++ src/main/java/io/github/JavaProject_DauphineM1_2019/Rules/Rules.java
@@ -3,11 +3,8 @@ package io.github.JavaProject_DauphineM1_2019.Rules;
 import java.nio.charset.StandardCharsets;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import org.apache.commons.lang3.RandomStringUtils;
 
 import com.google.common.hash.Hashing;
-
-import io.github.JavaProject_DauphineM1_2019.App;
 
 public class Rules {
 
@@ -15,7 +12,7 @@ public class Rules {
 	 * This method check if the parameter is a String
 	 * 
 	 * @param data
-	 * @return
+	 * @return boolean
 	 */
 	public boolean STRING(String data) {
 		return data instanceof String;
@@ -25,7 +22,7 @@ public class Rules {
 	 * This method check if the parameter is an Integer
 	 * 
 	 * @param data
-	 * @return
+	 * @return boolean
 	 */
 	public boolean INT(String data) {
 		try {
@@ -40,7 +37,7 @@ public class Rules {
 	 * This method check if the parameter is an Integer and in the interval ]0;120[
 	 * 
 	 * @param data
-	 * @return
+	 * @return boolean
 	 */
 	public boolean BE_AN_AGE(String data) {
 		return this.INT(data) && Integer.parseInt(data) > 0 && Integer.parseInt(data) < 120;
@@ -50,7 +47,7 @@ public class Rules {
 	 * This method check if the parameter respect the regEx of a mail (XXX@YYY.ZZ)
 	 * 
 	 * @param data
-	 * @return
+	 * @return boolean
 	 */
 	public boolean BE_AN_EMAIL(String data) {
 		String regex = "^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z]+\\.)+[a-zA-Z]{2,6}$";
@@ -64,7 +61,7 @@ public class Rules {
 	 * (XXX@dauphine.eu or XXX@dauphine.psl.eu or XXX@lamsade.dauphine.fr)
 	 * 
 	 * @param data
-	 * @return
+	 * @return boolean
 	 */
 	public boolean BE_AN_DAUPHINE_EMAIL(String data) {
 		String regexDauphine = "^(([\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(dauphine.eu|dauphine.psl.eu|lamsade.dauphine.fr)))$";
@@ -73,14 +70,27 @@ public class Rules {
 		return mat.matches();
 	}
 	
-	public String RANDOM_LETTER(String s) {
-		return Hashing.sha256().hashString(s, StandardCharsets.UTF_8).toString().substring(0, s.length());
+	/**
+	 * This method replace each letter of a string with another letter
+	 * with the help of https://mkyong.com/java/java-md5-hashing-example/ And https://www.baeldung.com/sha-256-hashing-java
+	 * 
+	 * @param data
+	 * @return String with random letter
+	 */
+	public String RANDOM_LETTER(String data) {
+		return Hashing.sha256().hashString(data, StandardCharsets.UTF_8).toString().substring(0, data.length());
 	}
 	
-	public String RANDOM_LETTER_FOR_LOCAL_PART(String stringToAnonymize){
-	    String anonymization= stringToAnonymize.substring(0, stringToAnonymize.indexOf("@"));
-	    String endString= stringToAnonymize.substring(stringToAnonymize.indexOf("@"));
-	    return RANDOM_LETTER(anonymization) + endString;
+	/**
+	 * This method replace each letter of the first part of a mail with another letter
+	 * 
+	 * @param data
+	 * @return data with the first part with random letter
+	 */
+	public String RANDOM_LETTER_FOR_LOCAL_PART(String data){
+	    String beginningString = data.substring(0, data.indexOf("@"));
+	    String endString = data.substring(data.indexOf("@"));
+	    return RANDOM_LETTER(beginningString) + endString;
 	}
 	
 }
