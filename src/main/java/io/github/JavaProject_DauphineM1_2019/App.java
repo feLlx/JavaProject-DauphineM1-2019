8b8d693efacbc4d815b790c6f16deaeb2ef28a7e
feLlx <vitasfelix@gmail.com>
2020-03-05 20:08:42 +0100

    Modification main for checking and anonymization together

--- src/main/java/io/github/JavaProject_DauphineM1_2019/App.java
+++ src/main/java/io/github/JavaProject_DauphineM1_2019/App.java
@@ -19,38 +19,74 @@ public class App {
 	}
 
 	public void proceed() throws IOException {
-		LOGGER.info("Hello user !");
-		LOGGER.info("In this app you can either :\n" +
+		LOGGER.info("Hello user ! \n" + 
+				"In this app you can either :\n" +
 				"1 - Check if a csv file respects the rules indicated in a json file\n" +
-				"2 - Anonymize the data in a csv file according to the rules indicated in a json file \n");
-		LOGGER.info("Please enter 1 or 2 depending on the task you wish to perform");
+				"2 - Anonymize the data in a csv file according to the rules indicated in a json file \n" +
+				"3 - Check and anonymize at the same time \n");
+		LOGGER.info("Please enter 1, 2 or 3 depending on the task you wish to perform");
 		String input = in.readLine();
-		if (Integer.parseInt(input) == 1 || Integer.parseInt(input) == 2) {
-			LOGGER.info("Please enter in this order and separated : \n"
-					+ "file name and extension of input file \n"
-					+ "file name and extension of description file \n"
-					+ "file name and extension of rule file \n"
-					+ "file name and extension of output file \n");
-			String[] line = new String[4]; 
-			line = in.readLine().split(" "); 
-			String inputFile = line[0];  
-			String descFile = line[1];  
-			String ruleFile = line[2];  
-			String outputFile = line[3];
+		try {
+			if (Integer.parseInt(input) == 1 || Integer.parseInt(input) == 2 || Integer.parseInt(input) == 3) {
+				String message = "Please enter in this order and separated with space : \n"
+						+ "file name and extension of input file \n"
+						+ "file name and extension of description file \n";
+				if (Integer.parseInt(input) != 3)
+					message += "file name and extension of rule file \n";
+				else
+					message += "file name and extension of rule file for checking \n"
+							+ "file name and extension of rule file for anonymization \n";
+				message += "file name and extension of output file \n";
 
-			if (Integer.parseInt(input) == 1) {
-				@SuppressWarnings("unused")
-				CheckData cd = new CheckData(inputFile, descFile, ruleFile, outputFile);	
+				LOGGER.info(message);
+				String[] line = new String[5]; 
+				line = in.readLine().split(" "); 
+				String inputFile = line[0];  
+				String descFile = line[1];
+				String ruleFile = line[2];
+				String outputFile = line[3];
+
+				if (Integer.parseInt(input) == 1) {
+					@SuppressWarnings("unused")
+					CheckData cd = new CheckData(inputFile, descFile, ruleFile, outputFile);	
+				}
+				else if (Integer.parseInt(input) == 2) {
+					@SuppressWarnings("unused")
+					AnonymizeData ad = new AnonymizeData(inputFile, descFile, ruleFile, outputFile);	
+				}
+				else if (Integer.parseInt(input) == 3) {
+					String checkRuleFile = line[2];  
+					String anonymRuleFile = line[3]; 
+					outputFile = line[4];
+
+					CheckData cd = new CheckData();
+					cd.getInvokeMethod(descFile, "name", "dataType");
+					cd.getInvokeMethod(checkRuleFile, "name", "should");
+					cd.readCsv(inputFile, "", ",", false);
+					AnonymizeData ad = new AnonymizeData();	
+					ad.getInvokeMethod(anonymRuleFile,"name","changeTo");
+					String temp ="";
+					String[] fieldsTemp = cd.getContentFileCsv().get(0);
+					for (int j = 0; j < fieldsTemp.length; j ++) {
+						if (ad.getContentMethod().containsKey(fieldsTemp[j]))
+							temp += fieldsTemp[j] + ",";
+					}
+					String[] fields = temp.split(",");
+					ad.getContentFileCsv().add(fields);
+					for (int i = 1; i < cd.getContentFileCsv().size(); i ++) {
+						String[] data = cd.getContentFileCsv().get(i);
+						ad.executeTask(fieldsTemp, data);
+					}
+					ad.writeCsv(outputFile);
+				}
+				LOGGER.info("Success");
+				LOGGER.info("Application closed");
 			}
 			else {
-				@SuppressWarnings("unused")
-				AnonymizeData ad = new AnonymizeData(inputFile, descFile, ruleFile, outputFile);	
+				LOGGER.error("Incorrect. Application closed");
 			}
-			LOGGER.info("Succes");
-			LOGGER.info("Application closed");
-		}
-		else {
-			LOGGER.info("Application closed");
+		} catch (Exception e) {
+			LOGGER.error("Failed. Application closed");
 		}
 	}
 }
